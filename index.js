74f4514357c36f903991dbb30c0aa147c95ea65a
poc <poc7667@gmail.com>
2016-08-09 21:28:57 -0700

    add redux version

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import App from './components/App.jsx';
+
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+import {createStore} from 'redux';
+import {changeAmount} from './redux/reducer';
+import {deposit, withdraw} from './redux/actions';
+
+let store = createStore(changeAmount, 1000);
+
+let unsubscribe = store.subscribe(() => {
+    console.log(store.getState()); 
+});
+
+store.dispatch(deposit(100));
+store.dispatch(deposit(200));
+store.dispatch(deposit(300));
+store.dispatch(withdraw(50));
+store.dispatch(withdraw(150));
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+unsubscribe();
