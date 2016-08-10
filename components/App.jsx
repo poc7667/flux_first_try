9ae3dd5105fc90a48f855bcf041952b815709f21
poc <poc7667@gmail.com>
2016-08-09 20:16:25 -0700

    first component

---
+++ components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends  React.Component {
+    render(){
+        return(
+            <div>
+                <header>UCSC BANK</header>
+            </div>
+        )
+    }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: [
-        'webpack-dev-server/client?https://0.0.0.0:8080',
+        'webpack-dev-server/client?http://0.0.0.0:8080',
         'webpack/hot/only-dev-server',
         './index.js'
     ],
